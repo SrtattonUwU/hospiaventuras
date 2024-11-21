baef88de97b478a44f7b380124b4feeba8313490
SrtattonUwU <santiagoescobarm2005@gmail.com>
2024-11-21 18:48:14 -0500

    nuevos cambios

--- PHP/register.php
+++ PHP/register.php
@@ -1,11 +1,5 @@
 <?php
 session_start();
-
-if (!isset($_SESSION['user_id'])) {
-    header('Location: Login.php');
-    exit();
-}
-
 $server = 'localhost:3306'; 
 $username = 'root';
 $password = '';
@@ -13,19 +7,38 @@ $database = 'hospiaventuras';
 
 try {
     $con = new PDO("mysql:host=$server;dbname=$database;", $username, $password);
+    $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    $user_id = $_SESSION['user_id'];
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        $username = $_POST['username'];
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+        $repeatPassword = $_POST['repeatPassword'];
 
-    $stmt = $con->prepare("SELECT * FROM usuarios WHERE id = :user_id");
-    $stmt->bindParam(':user_id', $user_id);
-    $stmt->execute();
-    
-    if ($stmt->rowCount() > 0) {
-        $user_data = $stmt->fetch(PDO::FETCH_ASSOC);
-    } else {
-        die("Usuario no encontrado.");
+        if ($password !== $repeatPassword) {
+            header('Location: register.html?error=password_mismatch');
+            exit();
+        }
+
+        $stmt = $con->prepare("SELECT * FROM usuarios WHERE email = :email OR username = :username");
+        $stmt->bindParam(':email', $email);
+        $stmt->bindParam(':username', $username);
+        $stmt->execute();
+
+        if ($stmt->rowCount() > 0) {
+            header('Location: ../html/Register.html?error=user_exists');
+            exit();
+        }
+
+        $stmt = $con->prepare("INSERT INTO usuarios (username, email, password) VALUES (:username, :email, :password)");
+        $stmt->bindParam(':username', $username);
+        $stmt->bindParam(':email', $email);
+        $stmt->bindParam(':password', $password);
+        $stmt->execute();
+
+        header('Location: ../html/Login.html');
+        exit();
     }
-
 } catch (PDOException $e) {
     die('Error de conexión: ' . $e->getMessage());
 }
